6c7f0f598e9fd549dfdf48ec13c37233debb6a2c
C1329353 Abner Fonseca <abner.fonseca@castgroup.com.br>
2025-06-21 16:32:10 -0300

    Fragment Config ajustado

--- app/src/main/java/com/study/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
+++ app/src/main/java/com/study/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
@@ -1,22 +1,65 @@
 package com.study.sorteadordenumeros
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.core.widget.addTextChangedListener
+import com.study.sorteadordenumeros.databinding.FragmentConfiguracaoDeSorteioBinding
 
 class ConfiguracaoDeSorteioFragment : Fragment() {
 
+    private var _binding: FragmentConfiguracaoDeSorteioBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracao_de_sorteio, container, false)
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConfiguracaoDeSorteioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            swtNotRepeatNumbers.setOnCheckedChangeListener { _, isChecked ->
+                swtNotRepeatNumbers.trackTintList = getColorStateList(
+                    requireContext(),
+                    if (isChecked) R.color.background_brand else R.color.content_tertiary
+                )
+            }
+            etAmoutNumbers.addTextChangedListener { text ->
+                //Todo: Validar se o texto é um número válido
+                Log.d(
+                    "ConfiguracaoDeSorteioFragment", "Valor do campo de texto: ${text.toString()}"
+                )
+            }
+
+            etInitialLimit.addTextChangedListener { text ->
+                //Todo: Validar se o texto é um número válido
+
+                Log.d(
+                    "ConfiguracaoDeSorteioFragment", "Valor do campo de texto: ${text.toString()}"
+                )
+            }
+
+            etFinalLimit.addTextChangedListener { text ->
+                //Todo: Validar se o texto é um número válido
+                Log.d(
+                    "ConfiguracaoDeSorteioFragment", "Valor do campo de texto: ${text.toString()}"
+                )
+            }
 
 
+        }
+    }
 }
--- app/src/main/java/com/study/sorteadordenumeros/MainActivity.kt
+++ app/src/main/java/com/study/sorteadordenumeros/MainActivity.kt
@@ -3,18 +3,68 @@ package com.study.sorteadordenumeros
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.study.sorteadordenumeros.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcvContent) as? NavHostFragment
+        navHostFragment?.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        with(binding) {
+            btnDraw.setOnClickListener {
+                //TODO: Navegar para proxima tela sortear ou voltar para anterior sortear novamente
+                when (btnDraw.text) {
+                    getString(R.string.sortear) -> {
+                        navController?.navigate(R.id.action_configuracaoDeSorteioFragment_to_resultadoSorteioFragment)
+                        btnDraw.apply {
+                            text = getString(R.string.sortear_novamente)
+                            btnDraw.setCompoundDrawablesWithIntrinsicBounds(
+                                null, null,
+                                AppCompatResources.getDrawable(
+                                    this@MainActivity,
+                                    R.drawable.ic_sortear_novamente
+                                ), null
+                            )
+                        }
+                    }
+
+                    getString(R.string.sortear_novamente) -> {
+                        navController?.popBackStack()
+                        btnDraw.apply {
+                            text = getString(R.string.sortear)
+                            btnDraw.setCompoundDrawablesWithIntrinsicBounds(
+                                null, null,
+                                AppCompatResources.getDrawable(
+                                    this@MainActivity,
+                                    R.drawable.ic_ir
+                                ), null
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
